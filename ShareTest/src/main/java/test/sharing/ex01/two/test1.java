8c6065044857cd362d9aa82ebcc4c0f19bdbe1cc
reako99 <zldrgx@naver.com>
2023-10-13 15:45:34 +0900

    add to test1

---
+++ ShareTest/src/main/java/test/sharing/ex01/two/test1.java
@@ -0,0 +1,31 @@
+package test.sharing.ex01.two;
+
+public class test1 {
+	private String test;
+
+	public test1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public test1(String test) {
+		super();
+		this.test = test;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	@Override
+	public String toString() {
+		return "test1 [test=" + test + "]";
+	}
+	
+	
+
+}
